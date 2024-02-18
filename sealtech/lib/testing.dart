0636ac905127545548abf2754907e6fe75c84285
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 22:16:56 +0530

    modify: testing file

---
+++ sealtech/lib/testing.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sealtech/button.dart';
+
+import 'custom_button.dart'; // Make sure to import the custom button file
+
+class CustomPage extends StatelessWidget {
+  final String title;
+  final String bodyText;
+
+  CustomPage({required this.title, required this.bodyText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              bodyText,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            CustomButton(
+              onPressed: () {
+                // Put your code here
+              },
+              buttonText: 'Click Me',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
