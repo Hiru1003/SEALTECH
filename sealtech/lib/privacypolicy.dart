0f8c55ef7f207151c9ae7e900e99e1b0a623ab75
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 20:56:57 +0530

    feat: content

--- sealtech/lib/privacypolicy.dart
+++ sealtech/lib/privacypolicy.dart
@@ -9,8 +9,11 @@ class PrivacyPolicyPage extends StatelessWidget {
         centerTitle: true,
         toolbarHeight: kToolbarHeight + MediaQuery.of(context).padding.top,
       ),
-      body: Center(
-        child: Text('Privacy Policy content goes here'),
+      body: Padding(
+        padding: EdgeInsets.only(left: 16, right: 16),
+        child: Text('Lorem ipsum dolor sit amet consectetur. Diam tristique massa massa netus aliquet pellentesque consequat sagittis elit. Odio ipsum malesuada arcu fermentum eget egestas odio. Eget lobortis quis ultricies risus. In ut eget pharetra mauris bibendum condimentum. Vitae quis viverra id mattis eget mauris mattis. Ac consectetur vulputate interdum egestas. Arcu eget id tortor viverra integer elit mollis. Interdum arcu ut imperdiet turpis. Fermentum pharetra nam ut malesuada vestibulum massa. Nunc est arcu diam cursus. Dolor tincidunt pharetra congue maecenas. Morbi sapien mauris fermentum magna. Eget amet in proin sed. Etiam adipiscing diam diam odio. Habitasse feugiat turpis nulla commodo sed phasellus. Imperdiet purus malesuada libero duis sed. Volutpat etiam ac vitae ac. Purus augue nec porta quisque venenatis nascetur vulputate varius pulvinar. Ullamcorper diam mattis lorem nulla donec at neque pharetra egestas. Dolor eu maecenas enim tincidunt blandit tincidunt viverra semper tortor. Enim leo posuere congue vitae turpis bibendum imperdiet tellus. Sit sed pulvinar vel vitae auctor. Eget sapien ultricies viverra nec. Consectetur pulvinar sed consequat et quam. Sodales pulvinar ut mauris id quis. Erat at in diam auctor. Velit duis bibendum ut massa. Et elementum in nisi pharetra et proin euismod lectus. Sed vivamus aliquet at auctor amet ut molestie. Facilisis quis bibendum eu commodo at ultrices eget nulla nulla. Rhoncus ac interdum venenatis viverra quis pellentesque pretium ornare ac. In leo nisl purus faucibus.',
+        textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
