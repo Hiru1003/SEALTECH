9b8c24686bd097d20d285b4233e1656f1b4398f4
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 20:15:38 +0530

    modify: secondary colors

--- sealtech/lib/components/theme.dart
+++ sealtech/lib/components/theme.dart
@@ -6,3 +6,8 @@ var primaryColor = Color.fromRGBO(255, 132, 0, 1);
 var primary75 = Color.fromRGBO(255, 162, 61, 1);
 var primary50 = Color.fromRGBO(255, 192, 122, 1);
 var primary25 = Color.fromRGBO(255, 222, 184, 1);
+
+var secondaryColor = Color.fromRGBO(255, 245, 214, 1);
+var secondary75 = Color.fromRGBO(255, 247, 221, 1);
+var secondary50 = Color.fromRGBO(255, 248, 229, 1);
+var secondary25 = Color.fromRGBO(255, 250, 237, 1);
