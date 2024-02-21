229afe3b86bfdf99e8adc6dbf691270c34db914e
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 20:52:10 +0530

    feat: icon and text

--- sealtech/lib/Employee/Home.dart
+++ sealtech/lib/Employee/Home.dart
@@ -129,7 +129,12 @@ class _Home_PageState extends State<Home_Page> {
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center, children: [])),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.calendar_month),
+                      SizedBox(height: 5),
+                      Text('Holidays'),
+                    ])),
           ])
         ])));
   }
