a81795d6c1a75a84ab40ac3c2fbcc88b7d539c42
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 14:51:21 +0530

    feat: loading page with brand name

---
+++ sealtech/lib/loadingPage2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sealtech/components/theme.dart';
+
+class LoadingPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Center(
+        child: Image.asset('lib/images/logo-word-no-background .png', width: 60,),
+      ),
+    );
+  }
+}
