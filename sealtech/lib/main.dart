933a58ae3fd1a2d3ae302e751183b3861524d0b5
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 14:24:37 +0530

    fix: confilcts

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/Employee/Home.dart';
+import 'package:sealtech/testing.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home_Page(),
+      home: CustomPage(title: 'title', bodyText: 'bodyText'),
     );
   }
 }
