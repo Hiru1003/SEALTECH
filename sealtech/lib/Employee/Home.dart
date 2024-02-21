0dea8f15c390b4b99879e3d95f66edfb72bc2e94
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 20:52:49 +0530

    style:container 2

--- sealtech/lib/Employee/Home.dart
+++ sealtech/lib/Employee/Home.dart
@@ -128,13 +128,22 @@ class _Home_PageState extends State<Home_Page> {
                   ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Column(
+                child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(Icons.calendar_month),
                       SizedBox(height: 5),
                       Text('Holidays'),
                     ])),
+            SizedBox(width: 20), // Add space between containers
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
           ])
         ])));
   }
