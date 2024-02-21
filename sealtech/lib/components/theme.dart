f1e8d995abfc3854a3cc16455bdb4d802ec87f1a
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 20:12:26 +0530

    modify: primary colors

--- sealtech/lib/components/theme.dart
+++ sealtech/lib/components/theme.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
 var bgColor = Color.fromRGBO(255, 252, 245, 1);
+
+var primaryColor = Color.fromRGBO(255, 132, 0, 1);
+var primary75 = Color.fromRGBO(255, 162, 61, 1);
+var primary50 = Color.fromRGBO(255, 192, 122, 1);
+var primary25 = Color.fromRGBO(255, 222, 184, 1);
