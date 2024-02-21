77a6d7ae05417fd28c5aea8e5f0351b0f04952ba
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 22:58:02 +0530

    feat: sign up

--- sealtech/lib/signup.dart
+++ sealtech/lib/signup.dart
@@ -15,7 +15,7 @@ class SignUpPage extends StatelessWidget {
               
               //logo
               Padding(
-                padding: const EdgeInsets.only(top: 100),
+                padding: const EdgeInsets.only(top: 65),
                 child: Image.asset('lib/images/logo-no-background.png', width: 60,),
               ),
               const SizedBox(height: 60),
@@ -33,6 +33,26 @@ class SignUpPage extends StatelessWidget {
                       fontFamily: 'inter',
                     ),
                   ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              //name field
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: '   Name',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  style: TextStyle(color: Theme.of(context).primaryColor),
                 ),
               ),
               const SizedBox(height: 20),
@@ -113,7 +133,7 @@ class SignUpPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
-                      'New to SEALTECH?',
+                      'Already have an account?',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 13,
@@ -124,7 +144,7 @@ class SignUpPage extends StatelessWidget {
                         
                       },
                       child: const Text(
-                        'Sign Up',
+                        'Sign In',
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 14,
