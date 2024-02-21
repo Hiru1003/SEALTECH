5b3cb41e49486a49afbfd0f44fcace0180182513
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 14:07:26 +0530

    modify: main

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -12,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CustomPage(
-        title: 'Custom Page',
-        bodyText: 'This is a custom page.',
-      ),
+      home: CustomPage(title: 'title', bodyText: 'bodyText'),
     );
   }
 }
