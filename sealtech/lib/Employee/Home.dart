535f92a5d6efd1ae2b05685d92537373d403310e
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 19:03:48 +0530

    style: container

--- sealtech/lib/Employee/Home.dart
+++ sealtech/lib/Employee/Home.dart
@@ -13,37 +13,37 @@ class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'lib/images/logo-word-no-background.png',
+                  height: 20,
+                )
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'lib/images/logo-word-no-background.png',
-                height: 20,
-              )
-            ],
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
           ),
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0.00,
+          backgroundColor: Color.fromRGBO(251, 147, 0, 1),
         ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0.00,
-        backgroundColor: Color.fromRGBO(251, 147, 0, 1),
-      ),
-      backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
-    );
+        backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
+        body: SingleChildScrollView(child: Column(children: [Container()])));
   }
 }
