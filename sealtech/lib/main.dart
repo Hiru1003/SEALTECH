f3292b78db049ca17afae3d48bd4e7638f2da1ab
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 22:09:35 +0530

    feat: logo

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/privacypolicy.dart';
+import 'package:sealtech/signin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       
       debugShowCheckedModeBanner: false,
-      home: PrivacyPolicyPage(),
+      home: SignInPage(),
     );
   }
 }
