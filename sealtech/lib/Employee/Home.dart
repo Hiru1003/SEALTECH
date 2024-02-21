996a5c79351e46b059487a26c39147527f4736d1
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 20:50:19 +0530

    style: center

--- sealtech/lib/Employee/Home.dart
+++ sealtech/lib/Employee/Home.dart
@@ -120,9 +120,16 @@ class _Home_PageState extends State<Home_Page> {
           SizedBox(height: 15),
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             Container(
-              width: 100,
-              height: 100,
-            )
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 245, 214, 87),
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center, children: [])),
           ])
         ])));
   }
