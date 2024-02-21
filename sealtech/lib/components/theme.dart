9a8ec91770c111867197d890b328c034f7eacc27
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 20:19:21 +0530

    modify: accent colors

--- sealtech/lib/components/theme.dart
+++ sealtech/lib/components/theme.dart
@@ -11,3 +11,8 @@ var secondaryColor = Color.fromRGBO(255, 245, 214, 1);
 var secondary75 = Color.fromRGBO(255, 247, 221, 1);
 var secondary50 = Color.fromRGBO(255, 248, 229, 1);
 var secondary25 = Color.fromRGBO(255, 250, 237, 1);
+
+var accentColor = Color.fromRGBO(144, 53, 14, 1);
+var accent75 = Color.fromRGBO(172, 103, 71, 1);
+var accent50 = Color.fromRGBO(199, 152, 129, 1);
+var accent25 = Color.fromRGBO(227, 202, 187, 1);
