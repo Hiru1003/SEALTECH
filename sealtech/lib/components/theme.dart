453dbd031a79b2e5bd105656a2ee4a584c6d7f4f
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 14:46:33 +0530

    style: loading page 1

---
+++ sealtech/lib/components/theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var bgColor = Color.fromRGBO(255, 252, 245, 1);
--- sealtech/lib/loadingpage1.dart
+++ sealtech/lib/loadingpage1.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sealtech/components/theme.dart';
 
 class LoadingPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 243, 142, 33),
+      backgroundColor: bgColor,
       body: Center(
-        child: Image.asset('lib/images/logo-no-background.png'),
+        child: Image.asset('lib/images/logo-no-background.png', width: 60,),
       ),
     );
   }
