460affb73e0aea97c83e65fd7f648c15af9402d3
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 22:53:09 +0530

    feat: sign up text

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/signin.dart';
+import 'package:sealtech/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+      home: SignUpPage(),
     );
   }
 }
---
+++ sealtech/lib/signup.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:sealtech/components/button.dart';
+import 'package:sealtech/components/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              
+              //logo
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Image.asset('lib/images/logo-no-background.png', width: 60,),
+              ),
+              const SizedBox(height: 60),
+
+              //sign up text
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Create an Account, Let’s Get Started!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'inter',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              //email and password fields
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: '   Email',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: '   Password',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ),
+
+              //forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+
+                    },
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 110, 110, 110),
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              
+              const SizedBox(height:20),
+
+              //sign in button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Button(
+                  buttonText: 'Sign In',
+                  onPressed: () {
+                    
+                  },
+                  color: 'orange',
+                  enableIcon: false,
+                  isStroked: false,
+                ),
+              ),
+
+              //new to sealtech
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'New to SEALTECH?',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
