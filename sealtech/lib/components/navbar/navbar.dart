514247528c1ff99d4eb8bc311f241f65c1153051
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-22 09:40:41 +0530

    feat: initial widget

---
+++ sealtech/lib/components/navbar/navbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//Initial widget
+class InitalScreenWidget extends StatefulWidget{
+  // InitialScreenWidget({Key key}) : super(key: key);
+
+  @override
+  HomeWidget createState() => HomeWidget();
+    
+}
