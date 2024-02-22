c59ba147006faf2d3a4892d79d20ee57ef0cac94
Hiru1003 <hirumiperera10@gmail.com>
2024-02-22 20:12:17 +0530

    modify: bg color

--- sealtech/lib/Employee/LocationDetailed1.dart
+++ sealtech/lib/Employee/LocationDetailed1.dart
@@ -15,38 +15,39 @@ class _LocationDetailed1_PageState extends State<LocationDetailed1_Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Ongoing Locations',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600),
-              ),
-            ],
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Ongoing Locations',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
           ),
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0.00,
+          backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
         ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0.00,
-        backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
-      ),
-    );
+        backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
+        body: SingleChildScrollView());
   }
 }
