00c875a525fa6f9951564c823e3347749b92e111
Hiru1003 <hirumiperera10@gmail.com>
2024-02-22 19:43:31 +0530

    feat: create location detailed page

---
+++ sealtech/lib/Employee/LocationDetailed1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sealtech/components/theme.dart';
+
+class LocationDetailed1_Page extends StatefulWidget {
+  const LocationDetailed1_Page({
+    super.key,
+  });
+
+  @override
+  State<LocationDetailed1_Page> createState() => _LocationDetailed1_PageState();
+}
+
+class _LocationDetailed1_PageState extends State<LocationDetailed1_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
