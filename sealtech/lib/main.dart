ac8c4e4b2585fbe9c54638986e0ea4f0e414c2cd
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-22 11:37:17 +0530

    modify: main

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/components/navbar/navbar.dart';
+import 'package:sealtech/Employee/Home.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: InitalScreenWidget(),
+      //home: SignUpPage(),
+      home: Home_Page(),
     );
   }
 }
