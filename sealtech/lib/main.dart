9bf0af384453f6d8053ce4ba8de0bfb0fdc5ceb3
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-22 09:23:06 +0530

    modify: privacy and security content

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/signup.dart';
+import 'package:sealtech/privacypolicy.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      home: PrivacyPolicyPage(),
     );
   }
 }
