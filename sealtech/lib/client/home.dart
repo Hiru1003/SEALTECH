9537fdaa84aa40955259954d35f925e952002751
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-23 13:47:19 +0530

    feat: appbar home client

--- sealtech/lib/client/home.dart
+++ sealtech/lib/client/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sealtech/components/theme.dart';
 
-
-class Home extends StatelessWidget{
-  Widget build(BuildContext context) => new Center(
-    child:  Container(
-      child: Text('This is homepage'),
-    )
-  );
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Center(
+            child: Image.asset('lib/images/logo-word-no-background.png', width: 160,),
+          ),
+          backgroundColor: primary75,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('This is homepage'),
+          ),
+        ),
+      );
 }
