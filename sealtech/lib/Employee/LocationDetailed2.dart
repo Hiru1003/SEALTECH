a1a0fc8a78e4429a08f47ce0711b30dfcb897336
Hiru1003 <hirumiperera10@gmail.com>
2024-02-23 10:31:05 +0530

    feat: payment

--- sealtech/lib/Employee/LocationDetailed2.dart
+++ sealtech/lib/Employee/LocationDetailed2.dart
@@ -170,6 +170,33 @@ class _LocationDetailed2_PageState extends State<LocationDetailed2_Page> {
                               ),
                             ],
                           ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 15),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Icon(
+                                    Icons.wallet,
+                                    color: Color.fromARGB(255, 59, 57, 57),
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 35),
+                              Expanded(
+                                child: Text(
+                                  'Due Payment',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    color: Color.fromARGB(255, 96, 94, 94),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
