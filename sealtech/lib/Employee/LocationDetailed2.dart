24057bba5c68ffcadc098f6fe485b509d6b5e248
Hiru1003 <hirumiperera10@gmail.com>
2024-02-23 09:49:11 +0530

    feat: scaffold

---
+++ sealtech/lib/Employee/LocationDetailed2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sealtech/components/theme.dart';
+
+class LocationDetailed2_Page extends StatefulWidget {
+  const LocationDetailed2_Page({
+    super.key,
+  });
+
+  @override
+  State<LocationDetailed2_Page> createState() => _LocationDetailed2_PageState();
+}
+
+class _LocationDetailed2_PageState extends State<LocationDetailed2_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
