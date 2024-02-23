777b140e303fbf7bcfef97c438f41291c51e5926
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-23 14:38:46 +0530

    feat: home client popular services and essentials

--- sealtech/lib/client/home.dart
+++ sealtech/lib/client/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sealtech/components/button.dart';
 import 'package:sealtech/components/theme.dart';
 
@@ -11,54 +12,77 @@ class Home extends StatelessWidget {
           ),
           backgroundColor: primary75,
         ),
-        body: Stack(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 180,
-              child: Container(
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 180,
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage('lib/images/home_client.png',),
                     fit: BoxFit.cover,
                   ),
                 ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 8,
-                      right: 16,
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              'Discover Unrivaled\nWaterproofing Services\nwith SealTech!',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.right,
-                            ),
-                            Button(
-                              buttonText: 'More',
-                              onPressed: (){},
-                              enableIcon: true,
-                              color: 'black',
-                              isStroked: true,
-                              width: 150,
-                            )
-                          ],
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(height: 8,),
+                        Text(
+                          'Discover Unrivaled\nWaterproofing Services\nwith SealTech!',
+                          style: GoogleFonts.inter(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.right,
                         ),
-                      ),
+                        Button(
+                          buttonText: 'More',
+                          onPressed: (){},
+                          enableIcon: true,
+                          color: 'black',
+                          isStroked: true,
+                          width: 150,
+                        )
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 16,),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 16), // Add padding for left
+                child: RichText(
+                  text: TextSpan(
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Popular',
+                        style: TextStyle(
+                          color: primaryColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' Services and Essentials',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
 }
