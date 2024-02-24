acca82ad95791a7bc2c96b391790bc26646533b0
Hiru1003 <hirumiperera10@gmail.com>
2024-02-24 07:54:29 +0530

    feat: scaffold

---
+++ sealtech/lib/Employee/Holidays.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Holiday_Page extends StatefulWidget {
+  const Holiday_Page({
+    super.key,
+  });
+
+  @override
+  State<Holiday_Page> createState() => _Holiday_PageState();
+}
+
+class _Holiday_PageState extends State<Holiday_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
