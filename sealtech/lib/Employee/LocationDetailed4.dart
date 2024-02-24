bbddeb30617bb43d8c7ab728efc29bdcc82cf041
Hiru1003 <hirumiperera10@gmail.com>
2024-02-24 07:49:35 +0530

    feat: cheackboxes

--- sealtech/lib/Employee/LocationDetailed4.dart
+++ sealtech/lib/Employee/LocationDetailed4.dart
@@ -218,6 +218,98 @@ class _LocationDetailed4_PageState extends State<LocationDetailed4_Page> {
                       ),
                     ],
                   ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 10),
+                        MyCheckBoxContainer(title: 'Visit Site'),
+                        const SizedBox(height: 10),
+                        MyCheckBoxContainer(title: 'Give Quatation'),
+                        const SizedBox(height: 10),
+                        MyCheckBoxContainer(title: 'Start Work'),
+                        const SizedBox(height: 10),
+                        MyCheckBoxContainer(title: 'Completion'),
+                        const SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
                 ]))));
+  }
+}
+
+class MyCheckBoxContainer extends StatefulWidget {
+  final String title;
+
+  MyCheckBoxContainer({required this.title});
+
+  @override
+  _MyCheckBoxContainerState createState() => _MyCheckBoxContainerState();
+}
+
+class _MyCheckBoxContainerState extends State<MyCheckBoxContainer> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isChecked = !isChecked;
+        });
+      },
+      child: Container(
+        width: 330,
+        height: 45,
+        padding: const EdgeInsets.only(left: 15),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          children: [
+            const SizedBox(width: 25),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: isChecked
+                      ? primaryColor
+                      : const Color.fromRGBO(255, 132, 0, 1),
+                  width: 2.0,
+                ),
+              ),
+              child: CircleAvatar(
+                radius: 15,
+                backgroundColor: isChecked
+                    ? primaryColor
+                    : const Color.fromRGBO(255, 252, 245, 1),
+                child: isChecked
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 15.0,
+                      )
+                    : null,
+              ),
+            ),
+            //
+            const SizedBox(width: 30),
+            Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Color.fromARGB(255, 96, 94, 94),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
