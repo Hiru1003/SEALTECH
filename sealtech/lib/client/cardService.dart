6eaaa984629693055368baf1c21a6aba707f31c2
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-25 20:27:53 +0530

    feat: dot indicators

--- sealtech/lib/client/cardService.dart
+++ sealtech/lib/client/cardService.dart
@@ -1,6 +1,19 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:sealtech/components/theme.dart';
 
-class ServiceCard extends StatelessWidget {
+class ServiceCard extends StatefulWidget {
+  ServiceCard({Key? key}) : super(key: key);
+
+  @override
+  ServiceCardState createState() => ServiceCardState();
+}
+
+class ServiceCardState extends State<ServiceCard> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -9,7 +22,7 @@ class ServiceCard extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
-          title: Text('Product Info'),
+          title: const Text('Product Info'),
           actions: [
             IconButton(
               icon: Image.asset('lib/images/logoIconBlack.png'),
@@ -17,23 +30,41 @@ class ServiceCard extends StatelessWidget {
             ),
           ],
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: PageView(
-                  children: [
-                    Image.asset('lib/images/proInfo.png'),
-                    Image.asset('lib/images/proInfo.png'),
-                    Image.asset('lib/images/proInfo.png'),
-                  ],
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                children: [
+                  Image.asset('lib/images/proInfo.png'),
+                  Image.asset('lib/images/proInfo.png'),
+                  Image.asset('lib/images/proInfo.png'),
+                  Image.asset('lib/images/proInfo.png'),
+                ],
+              ),
+            ),
+            DotsIndicator(
+              dotsCount: 4,
+              position: _currentPage,
+              decorator: DotsDecorator(
+                activeColor: accent75,
+                activeSize: const Size(48, 12),
+                activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
                 ),
+                spacing: const EdgeInsets.all(4),
               ),
-              Container(
-                child: Text('This is ServiceCards page'),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(height:32),
+             Text('First line of text'),
+             Text('Third line of text'),
+             Text('Third line of text'),
+          ],
         ),
       );
 }
