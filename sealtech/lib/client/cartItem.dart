e92fe90b74db0dd1813fe4934325d53b52c37e4f
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 19:27:18 +0530

    feat: texts

--- sealtech/lib/client/cartItem.dart
+++ sealtech/lib/client/cartItem.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sealtech/components/theme.dart';
 
 class CartItem extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
@@ -8,13 +10,58 @@ class CartItem extends StatelessWidget {
             children: [
               Padding(padding: EdgeInsets.only(bottom: 208), child: Image.asset('lib/images/pro1.png')),
               Padding(
-                padding: EdgeInsets.only(top: 208.0, left: 16.0),
+                padding: EdgeInsets.only(top: 178.0, left: 16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Text 1'),
-                    Text('Text 2'),
-                    Text('Text 3'),
+                    Text(
+                      'Swimming Pool (8ft)',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text('Lorem ipsum dolor sit\namet consectetur...'),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: GoogleFonts.inter(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'Price:',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '6,200 LKR',
+                                style: TextStyle(
+                                  color: primaryColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 24,),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'Additional Text',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
