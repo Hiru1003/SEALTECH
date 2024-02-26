f9dc60a5f54393eddeaf3ab6759639b1ffbf0530
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 22:05:23 +0530

    feat: delivery fee discount divider total price button

--- sealtech/lib/client/cart.dart
+++ sealtech/lib/client/cart.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sealtech/client/cartItem.dart';
+import 'package:sealtech/components/button.dart';
+import 'package:sealtech/components/theme.dart';
 
 class Cart extends StatefulWidget {
   Cart({Key? key}) : super(key: key); // Added named 'key' parameter
@@ -32,7 +34,7 @@ class CartState extends State<Cart> { // Made the class public
           ),
         ],
       ),
-      body: SingleChildScrollView( // Removed unnecessary Container
+      body: SingleChildScrollView(
         child: Column(
           children: [
             CartItem(
@@ -84,6 +86,95 @@ class CartState extends State<Cart> { // Made the class public
                 ),
               ],
             ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text( // Added 'const'
+                        'Delivery Fee',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Text('200 LKR'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text( // Added 'const'
+                        'Discount',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Text('0 LKR', style: TextStyle(color: Colors.grey[600])),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Divider(),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text( // Added 'const'
+                        'Total Price',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Text('5009999 LKR', style: TextStyle(fontWeight: FontWeight.w700, color: primaryColor, fontSize: 18)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 24),
+            Button(buttonText: 'Proceed to Checkout', onPressed: (){}, color: 'orange', enableIcon: true, width: 380,)
           ],
         ),
       ),
