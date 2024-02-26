d5b96f65a9f01fbf45fcba76a35071732e33b64f
Hiru1003 <hirumiperera10@gmail.com>
2024-02-26 08:09:09 +0530

    feat: scaffold

---
+++ sealtech/lib/Employee/SalaryCalculator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Salary_Page extends StatefulWidget {
+  const Salary_Page({
+    super.key,
+  });
+
+  @override
+  State<Salary_Page> createState() => _Salary_PageState();
+}
+
+class _Salary_PageState extends State<Salary_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
