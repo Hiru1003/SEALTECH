cd291794ae0335b7588182815240e65e649a920a
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 12:09:26 +0530

    feat: cart appbar

--- sealtech/lib/client/cart.dart
+++ sealtech/lib/client/cart.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-
-class Cart extends StatelessWidget{
-  Widget build(BuildContext context) => new Center(
-    child:  Container(
-      child: Text('This is Carts page'),
-    )
+class Cart extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Text('Cart'),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Image.asset('lib/images/logoIconBlack.png'),
+            ),
+          ],
+        ),
   );
 }
-
