3f1b230f8a617f67ae8a085a0f9d0b4c6994d9a9
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 19:59:02 +0530

    feat: increase decrease buttons

--- sealtech/lib/client/cartItem.dart
+++ sealtech/lib/client/cartItem.dart
@@ -3,70 +3,90 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:sealtech/components/theme.dart';
 
 class CartItem extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(padding: EdgeInsets.only(bottom: 208), child: Image.asset('lib/images/pro1.png')),
-              Padding(
-                padding: EdgeInsets.only(top: 178.0, left: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Swimming Pool (8ft)',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 100,
+              height: 110,
+              child: Image.asset('lib/images/pro1.png', fit: BoxFit.cover),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Swimming Pool (8ft)',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(height: 16),
-                    Text('Lorem ipsum dolor sit\namet consectetur...'),
-                    SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            style: GoogleFonts.inter(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: 'Price:',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              ),
-                              TextSpan(
-                                text: '6,200 LKR',
-                                style: TextStyle(
-                                  color: primaryColor,
-                                ),
-                              ),
-                            ],
+                  ),
+                  SizedBox(height: 16),
+                  Text('Lorem ipsum dolor sit\namet consectetur...'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: GoogleFonts.inter(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                           ),
+                          children: [
+                            TextSpan(
+                              text: 'Price:',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                            WidgetSpan(
+                              child: SizedBox(width: 13),
+                            ),
+                            TextSpan(
+                              text: '6,200 LKR',
+                              style: TextStyle(
+                                color: primaryColor,
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 24,),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            'Additional Text',
+                      ),
+                      SizedBox(width: 18,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.remove),
+                            onPressed: () {},
+                          ),
+                          Text(
+                            '1',
                             style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                          IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
