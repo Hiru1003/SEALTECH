a20dfbdd5f8dfa3e33dae71159d01fddb62b5bb7
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 10:47:02 +0530

    feat: appbar [search]

--- sealtech/lib/client/search.dart
+++ sealtech/lib/client/search.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sealtech/components/theme.dart';
 
-
-class Search extends StatelessWidget{
-  Widget build(BuildContext context) => new Center(
-    child:  Container(
-      child: Text('This is Search page'),
-    )
-   
+class Search extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Center(
+            child: Image.asset('lib/images/logo-word-no-background.png', width: 160,),
+          ),
+          backgroundColor: bgColor,
+        ),
   );
 }
