90f410537fceb3b8321bfcd6b476c11fee547ee3
Hiru1003 <hirumiperera10@gmail.com>
2024-02-26 08:16:17 +0530

    feat: image

--- sealtech/lib/Employee/SalaryCalculator.dart
+++ sealtech/lib/Employee/SalaryCalculator.dart
@@ -14,37 +14,54 @@ class _Salary_PageState extends State<Salary_Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Salary Calculator',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600),
-              ),
-            ],
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Salary Calculator',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
           ),
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0.00,
+          backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
         ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0.00,
-        backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
-      ),
-    );
+        backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.only(left: 0),
+                          child: Image.asset(
+                            'lib/images/salaryimage.png',
+                            height: 180,
+                          )),
+                    ],
+                  ),
+                ]))));
   }
 }
