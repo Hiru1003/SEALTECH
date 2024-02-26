7dfc73e81f1ad974aa2407988c54953f090f534c
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 22:24:11 +0530

    feat: profile appbar

--- sealtech/lib/client/profile.dart
+++ sealtech/lib/client/profile.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-
-class Profile extends StatelessWidget{
-  Widget build(BuildContext context) => new Center(
-    child:  Container(
-      child: Text('This is Profiles page'),
-    )
-   
+class Profile extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Text('Profile'),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Image.asset('lib/images/logoIconBlack.png'),
+            ),
+          ],
+        ),
   );
 }
