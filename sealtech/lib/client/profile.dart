9f52075d4f2c9a65e6089413d9a1feb692361197
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-26 22:45:42 +0530

    feat: invite friends [profile]

--- sealtech/lib/client/profile.dart
+++ sealtech/lib/client/profile.dart
@@ -138,6 +138,32 @@ class Profile extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(height: 16.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your button logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black,
+                    side: BorderSide(color: Colors.black, width: 0.4),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    elevation: 0,
+                    minimumSize: Size(120, 40),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Invite Friends'),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
