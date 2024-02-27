c18fd3dc1bdc21ce78970f75f8299715c510825f
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-27 10:21:29 +0530

    feat: image 2 + scrollable

--- sealtech/lib/client/aboutUs.dart
+++ sealtech/lib/client/aboutUs.dart
@@ -1,4 +1,3 @@
-// about_us.dart
 import 'package:flutter/material.dart';
 import 'package:sealtech/components/theme.dart';
 
@@ -15,33 +14,37 @@ class AboutUs extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0,),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 16,),
-            Text(
-              'Waterproof  |  Seal  |  Protection',
-              style: TextStyle(color: primaryColor),
-              textAlign: TextAlign.left,
-            ),
-            Text(
-              'About Our Company SEALTECH',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
-            ),
-            SizedBox(height: 20,),
-            Image.asset('lib/images/aboutus1.png'),
-            SizedBox(height: 20,),
-            Text(
-              "At SEALTECH, we pride ourselves on being a pioneering force in the waterproofing industry, dedicated to delivering top-notch solutions that exceed our clients' expectations. Established in 1995, SEALTECH emerged from humble beginnings with a vision to revolutionize waterproofing practices across residential, commercial, and industrial sectors.",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0,),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16,),
+              Text(
+                'Waterproof  |  Seal  |  Protection',
+                style: TextStyle(color: primaryColor),
+                textAlign: TextAlign.left,
               ),
-            )
-          ],
+              Text(
+                'About Our Company SEALTECH',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+              ),
+              SizedBox(height: 20,),
+              Image.asset('lib/images/aboutus1.png'),
+              SizedBox(height: 20,),
+              Text(
+                "At SEALTECH, we pride ourselves on being a pioneering force in the waterproofing industry, dedicated to delivering top-notch solutions that exceed our clients' expectations. Established in 1995, SEALTECH emerged from humble beginnings with a vision to revolutionize waterproofing practices across residential, commercial, and industrial sectors.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 20,),
+              Image.asset('lib/images/aboutus2.png'),
+            ],
+          ),
         ),
       ),
     );
