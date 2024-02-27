5f6039aac56c7e21243b6440b0427728f67d2bec
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-27 22:00:58 +0530

    feat: appbar [chat]

--- sealtech/lib/client/chat.dart
+++ sealtech/lib/client/chat.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-
-class Chat extends StatelessWidget{
-  Widget build(BuildContext context) => new Center(
-    child:  Container(
-      child: Text('This is Chats page'),
-    )
-   
-  );
+class Chat extends StatelessWidget {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text('Chat'),
+          actions: [
+            IconButton(
+              icon: Image.asset('lib/images/logoIconBlack.png'),
+              onPressed: () {},
+            ),
+          ]
+        ),
+      );
 }
