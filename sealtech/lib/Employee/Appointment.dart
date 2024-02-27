473afa79e08aa49f1ab98dba82af729d409e36d1
Hiru1003 <hirumiperera10@gmail.com>
2024-02-27 09:09:06 +0530

    modify: appointent with fuction to get user input and print in console

--- sealtech/lib/Employee/Appointment.dart
+++ sealtech/lib/Employee/Appointment.dart
@@ -5,163 +5,194 @@ import 'package:sealtech/components/theme.dart';
 
 class Appoinment_Page extends StatefulWidget {
   const Appoinment_Page({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<Appoinment_Page> createState() => _Appoinment_PageState();
 }
 
 class _Appoinment_PageState extends State<Appoinment_Page> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController appointmentWithController =
+      TextEditingController();
+  final TextEditingController dateController = TextEditingController();
+  final TextEditingController timeController = TextEditingController();
+  final TextEditingController durationController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Appointment',
-                  style: GoogleFonts.poppins(
-                      textStyle: Theme.of(context).textTheme.displayLarge,
-                      color: const Color.fromARGB(255, 70, 66, 68),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Appointment',
+                style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  color: const Color.fromARGB(255, 70, 66, 68),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          titleSpacing: 4.0,
-          toolbarHeight: 65,
-          toolbarOpacity: 0.9,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(18),
-                bottomLeft: Radius.circular(18)),
-          ),
-          iconTheme: const IconThemeData(
-            color: Colors.black,
-          ),
-          elevation: 0.00,
-          backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
         ),
-        backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
-        body: SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(left: 0),
-                          child: Image.asset(
-                            'lib/images/Appointment.png',
-                            height: 200,
-                          )),
-                    ],
-                  ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(18),
+            bottomLeft: Radius.circular(18),
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        elevation: 0.00,
+        backgroundColor: const Color.fromRGBO(251, 147, 0, 1),
+      ),
+      backgroundColor: const Color.fromRGBO(255, 252, 245, 1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: ' Your Name',
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: const UnderlineInputBorder(),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: primaryColor),
-                        ),
-                      ),
+                    padding: const EdgeInsets.only(left: 0),
+                    child: Image.asset(
+                      'lib/images/Appointment.png',
+                      height: 200,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: ' Who do you want to make appointment with?',
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: const UnderlineInputBorder(),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: primaryColor),
-                        ),
-                      ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Your Name',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: ' Available date',
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: const UnderlineInputBorder(),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: primaryColor),
-                        ),
-                      ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: appointmentWithController,
+                  decoration: InputDecoration(
+                    labelText: 'Who do you want to make an appointment with?',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        labelText: ' Available time',
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: const UnderlineInputBorder(),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: primaryColor),
-                        ),
-                      ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: dateController,
+                  decoration: InputDecoration(
+                    labelText: 'Available date',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: TextField(
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        labelText: ' Duration',
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        border: const UnderlineInputBorder(),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: primaryColor),
-                        ),
-                      ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: timeController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Available time',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Button(
-                      buttonText: 'Submit',
-                      onPressed: () {},
-                      color: 'orange',
-                      enableIcon: false,
-                      isStroked: false,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: durationController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Duration',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    border: const UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: primaryColor),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                ]))));
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Button(
+                  buttonText: 'Submit',
+                  onPressed: () {
+                    // Access the user input using the controllers
+                    String name = nameController.text;
+                    String appointmentWith = appointmentWithController.text;
+                    String date = dateController.text;
+                    String time = timeController.text;
+                    String duration = durationController.text;
+
+                    // Do something with the user input
+                    print(
+                        'Name: $name, Appointment with: $appointmentWith, Date: $date, Time: $time, Duration: $duration');
+                  },
+                  color: 'orange',
+                  enableIcon: false,
+                  isStroked: false,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
