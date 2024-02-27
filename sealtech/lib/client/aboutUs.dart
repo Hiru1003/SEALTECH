a6a229eec7410f0725352a410ddd495855290cb8
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-27 10:43:05 +0530

    style: rich text

--- sealtech/lib/client/aboutUs.dart
+++ sealtech/lib/client/aboutUs.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sealtech/components/theme.dart';
 
 class AboutUs extends StatelessWidget {
+  const AboutUs({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +11,14 @@ class AboutUs extends StatelessWidget {
         title: Text('About Us'),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 16.0),
+            padding: EdgeInsets.only(right: 16.0),
             child: Image.asset('lib/images/logoIconBlack.png'),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0,),
+          padding: EdgeInsets.only(left: 16.0, right: 16.0,),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -34,30 +36,69 @@ class AboutUs extends StatelessWidget {
               SizedBox(height: 20,),
               Image.asset('lib/images/aboutus1.png'),
               SizedBox(height: 20,),
-              Text(
-                "At SEALTECH, we pride ourselves on being a pioneering force in the waterproofing industry, dedicated to delivering top-notch solutions that exceed our clients' expectations. Established in 1995, SEALTECH emerged from humble beginnings with a vision to revolutionize waterproofing practices across residential, commercial, and industrial sectors.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "At SEALTECH, ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "we pride ourselves on being a pioneering force in the waterproofing industry, dedicated to delivering top-notch solutions that exceed our clients' expectations. Established in 1995, SEALTECH emerged from humble beginnings with a vision to revolutionize waterproofing practices across residential, commercial, and industrial sectors.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
+                textAlign: TextAlign.justify,
               ),
               SizedBox(height: 20,),
               Image.asset('lib/images/aboutus2.png'),
               SizedBox(height: 20,),
-              Text(
-                "Our journey began when our founder, John Smith, recognized a critical need for durable, long-lasting waterproofing solutions amidst the burgeoning construction industry. With a relentless pursuit of excellence and a commitment to innovation, SEALTECH quickly gained traction as a trusted name synonymous with quality and reliability.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Our journey ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "began when our founder, John Smith, recognized a critical need for durable, long-lasting waterproofing solutions amidst the burgeoning construction industry. With a relentless pursuit of excellence and a commitment to innovation, SEALTECH quickly gained traction as a trusted name synonymous with quality and reliability.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
+                textAlign: TextAlign.justify,
               ),
               SizedBox(height: 10,),
-              Text(
-                "Over the years, SEALTECH has evolved into a dynamic team of industry experts, equipped with cutting-edge technology and unmatched expertise to tackle even the most challenging waterproofing projects. Our unwavering dedication to research and development ensures that we stay ahead of the curve, continuously refining our techniques and embracing emerging trends to deliver unparalleled results.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Over the years, ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "SEALTECH has evolved into a dynamic team of industry experts, equipped with cutting-edge technology and unmatched expertise to tackle even the most challenging waterproofing projects. Our unwavering dedication to research and development ensures that we stay ahead of the curve, continuously refining our techniques and embracing emerging trends to deliver unparalleled results.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
+                textAlign: TextAlign.justify,
               ),
               SizedBox(height: 20,),
             ],
