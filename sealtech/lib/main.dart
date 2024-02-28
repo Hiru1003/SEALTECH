082ed176060b161ac3666776943738888cee6389
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-28 09:33:10 +0530

    modify: navigations home

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/signup.dart';
+import 'package:sealtech/client/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
-      //home: NavbarC(),
+      home: NavbarC(),
       //home: NavbarE(),
     );
   }
