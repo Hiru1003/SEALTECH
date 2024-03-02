8667ddadecc4754e8d2ffa77bb340b4c771be342
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-03-02 10:50:52 +0530

    feat: FutureBuilder => loading pages

--- sealtech/lib/main.dart
+++ sealtech/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sealtech/client/navbar.dart';
 import 'package:sealtech/components/theme.dart';
+import 'package:sealtech/loadingPage2.dart';
+import 'package:sealtech/loadingpage1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<void> getData() async {
+    await Future.delayed(Duration(seconds: 2));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +21,20 @@ class MyApp extends StatelessWidget {
         primaryColor: accent75,
         textSelectionTheme: TextSelectionThemeData(
           selectionHandleColor: accentColor,
-        cursorColor: accent75,
-      ),
+          cursorColor: accent75,
+        ),
       ),
       debugShowCheckedModeBanner: false,
-      home: NavbarC(),
-      //home: NavbarE(),
-      //home: SignInPage(),
+      home: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingPage1();
+          } else {
+            return LoadingPage2();
+          }
+        },
+      ),
     );
   }
 }
